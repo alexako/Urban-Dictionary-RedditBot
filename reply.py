ee1c4d98e5a09d8f248c9238a716948cd4c377c7
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-03-31 21:11:36 -0500

    Added sys module and refactored some stuff
    sys module is temporrary until I figure out what is causing the recursionError.

--- reply.py
+++ reply.py
@@ -2,6 +2,8 @@ import praw
 import urbanScrape as us
 import time
 import re
+import sys
+sys.setrecursionlimit(10000)
 
 reddit = praw.Reddit(client_id='',
                         client_secret='',
@@ -10,43 +12,37 @@ reddit = praw.Reddit(client_id='',
                         password='')
 
 
-def post():
 
-    subreddit = reddit.subreddit('testingground4bots')
-# reads through subreddit comments and finds wake word.
+subreddit = reddit.subreddit('testingground4bots')
+bot_summon = "!urbandefine"
+urban_url = "http://www.urbandictionary.com/define.php?term="
+
+
+def run_bot():
+    global errors
     for comment in subreddit.stream.comments():
-        try:
-            x = comment.body.split() #I might need to move this or make it gloabl
-# When the word is found it grabs the deffinition from urban scrape it replies to the comment with the proper deffinition
-            if bot_summon in comment.body:
-                comment.reply((us.read_definition(us.get_definition_link(str(x[1])))))
+        if bot_summon in comment.body:
+            try:
+                x = us.read_definition(urban_url + comment.body.split()[1])
+                comment.reply(x)
                 print("replied to comment")
-            else:
-                comment.reply("word could not be deffined")
-                print("word could not be deffined")
-#sleep mode if api requests maxxed out
-        except praw.exceptions.APIException as e:
-            if (e.error_type == "RATELIMIT"):
-                delay = re.search("(\d+) minutes?", e.message)
-                print(e.message)
+            except praw.exceptions.APIException as e:
+                if e.error_type == "RATELIMIT":
+                    delay = re.search("(\d+) minutes?", e.message)
+                    print(e.message)
 
-                if delay:
-                    delay_seconds = float(int(delay.group(1)) * 60)
-                    time.sleep(delay_seconds)
-                    post()
-                else:
-                    delay = re.search("(\d+) seconds", e.message)
-                    delay_seconds = float(delay.group(1))
-                    time.sleep(delay_seconds)
-                    post()
-# Kills script if it recives over 5 errors
-        except:
-            errors = errors + 1
-            if (errors > 5):
-                print("crashed ")
-                exit(1)
-post()
-
-
-
-
+                    if delay:
+                        delay_seconds = float(int(delay.group(1)) * 60)
+                        time.sleep(delay_seconds)
+                        run_bot()
+                    else:
+                        delay = re.search("(\d+) seconds", e.message)
+                        delay_seconds = float(delay.group(1))
+                        time.sleep(delay_seconds)
+                        run_bot()
+            except:
+                errors = errors + 1
+                if errors > 5:
+                    print("crashed ")
+                    exit(1)
+run_bot()
